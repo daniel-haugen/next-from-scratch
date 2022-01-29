a0289133d930bae6b57654f38534becfc70f09bf
Daniel Haugen <me@dnhaugen.com>
2022-01-29 12:42:34 -0800

    data flowing -- caught up to other repo

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
+import Link from "next/link"
+
+
 function HomePage() {
-  return <div>Welcome to the Skate App
-  </div>
+  return <>
+  <div>Welcome to the Skate App</div>
+  <Link href="/skateparks">
+  <a>all skateparks</a>
+  </Link>
+  </>
 }
 
 export default HomePage
---
+++ pages/skateparks.js
@@ -0,0 +1,44 @@
+import Link from "next/link"
+
+
+export async function getStaticProps(context) {
+  const res = await fetch("https://skateparks-wa.herokuapp.com/skateparks");
+  const data = await res.json();
+  if (!data) {
+    return {
+      notFound: true,
+    }
+  }
+  return {
+    props: { data },
+  };
+}
+
+function AllSkateparks(data) {
+  return <>
+  <div>Go Home</div>
+  <Link href="/">
+  <a>Home</a>
+  </Link>
+
+  <div>
+        {data.data.map((parks, index) => {
+          return (
+            <div key={index}>
+              <h3 id={index} >{parks.name}</h3>
+              <p>{parks.native_land}</p>
+              <p>Rain cover? {parks.rain_cover.toString()}</p>
+              <p>Does it have lights? {parks.night_light.toString()}</p>
+              <p>{parks.full_address}</p>
+            </div>
+          );
+        })}
+      </div>
+
+
+
+
+  </>
+}
+
+export default AllSkateparks
