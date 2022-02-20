a46143cba4c4bbcde4cf996067fd69d0df5d012a
Daniel Haugen <me@dnhaugen.com>
2022-02-19 17:42:02 -0800

    fixed pagination

--- components/Pagination/Pagination.tsx
+++ components/Pagination/Pagination.tsx
@@ -2,15 +2,23 @@ import { FC } from "react";
 import Link from "next/link";
 import s from "./Pagination.module.css";
 
-interface Link {
-  href: string;
-  label: string;
-}
+const Pagination: FC<{
+  page: number;
+  setPage: Function;
+  parkCount: number;
+  parksPerPage: number;
+}> = (props) => (
 
-const Pagination: FC = () => (
-  <div className='flex gap-10'>
-    <button className="bg-midnight text-white">Go Back</button>
-    <button className="bg-midnight text-white">Next Page</button>
+  <div className="bg-silver w-1/4">
+    {props.page === 1 ? null : (
+      <button className="bg-midnight text-white" onClick={() => props.setPage(props.page - 1)}>Go Back</button>
+    )}
+
+    {props.parkCount !== props.parksPerPage ? null : 
+      <button className="bg-midnight text-white float-right" onClick={() => props.setPage(props.page + 1)}>Next Page</button>
+    }
+
+
   </div>
 );
 
--- pages/skateparks.tsx
+++ pages/skateparks.tsx
@@ -1,9 +1,7 @@
-import Link from "next/link"
-import { FC } from "react"
-import Navbar from "../components/Navbar"
-import Card from "../components/Card"
-import { useEffect, useState } from "react"
-import Pagination from '../components/Pagination'
+import Navbar from "../components/Navbar";
+import Card from "../components/Card";
+import { useEffect, useState } from "react";
+import Pagination from "../components/Pagination";
 
 export async function getStaticProps(context) {
   const res = await fetch("https://skateparks-wa.herokuapp.com/sp-min");
@@ -18,34 +16,39 @@ export async function getStaticProps(context) {
   };
 }
 
-let pageCount = 0;
 
 function AllSkateparks({ data }) {
   const [allParks, setAllParks] = useState(data);
   const [parksPerPage, setParksPerPage] = useState(7);
   const [page, setPage] = useState(1);
 
-  const renderParks = () => {
-    const firstPageIndex = (page - 1) * parksPerPage;
-    const lastPageIndex = firstPageIndex + parksPerPage;
-    const parksToShow = allParks.slice(firstPageIndex, lastPageIndex);
-    pageCount = parksToShow.length;
+  let firstPageIndex = (page - 1) * parksPerPage;
+  let lastPageIndex = firstPageIndex + parksPerPage;
+  let parksToShow = allParks.slice(firstPageIndex, lastPageIndex);
+  let parkCount = parksToShow.length;
+ 
 
-    if (pageCount > 0) {
-      return parksToShow.map((parks, index) => {
-        return <Card key={index} name={parks.name} address={parks.address} native_land={parks.native_land} slug={parks.slug} />;
+  const renderParks = () => {
+    return parksToShow.map((parks, index) => {
+        return (
+          <Card
+            key={index}
+            name={parks.name}
+            address={parks.address}
+            native_land={parks.native_land}
+            slug={parks.slug}
+          />
+        );
       });
-    }
   };
 
   return (
     <>
       <Navbar />
-      <Pagination />
-      <div className="flex gap-5 flex-wrap justify-start align-items-center mt-6 ">{renderParks()}</div>
-         
-
-        
+      <Pagination page={page} setPage={setPage} parkCount={parkCount} parksPerPage={parksPerPage} />
+      <div className="flex gap-5 flex-wrap justify-start align-items-center mt-6 ">
+        {renderParks()}
+      </div>
     </>
   );
 }
