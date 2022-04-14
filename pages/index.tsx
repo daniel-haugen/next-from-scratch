b067086518c7eb9fcf785e8806b48ff42af213f4
Daniel Haugen <me@dnhaugen.com>
2022-04-13 22:46:16 -0700

    pwa mode

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,15 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import ExploreGallery from "../components/ExploreGallery";
+import Head from 'next/head'
 
 function HomePage() {
   return (
     <>
+    <Head>
+    <link rel="manifest" href="/manifest.json" />
+    <link rel="apple-touch-icon" href="/logo.png" />
+    </Head>
       <Navbar />
       <Hero />
       <ExploreGallery />
