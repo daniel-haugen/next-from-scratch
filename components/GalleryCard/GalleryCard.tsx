1109bf6945d8b5ef5cfed91d78e33cc53f6a20d1
daniel-SP <me@dnhaugen.com>
2022-04-18 00:08:53 -0700

    updateS

--- components/GalleryCard/GalleryCard.tsx
+++ components/GalleryCard/GalleryCard.tsx
@@ -13,8 +13,11 @@ const GalleryCard: FC<{ name: string; image: string; slug: string }> = (props) =
   <Link href={'/' + props.slug}>
   <a>
   <div className="mx-8 mb-6 border-2 rounded border-gray-light bg-white">
-    <div className="bg-grey h-32 md:h-64 grid items-center relative">
-      {/* <Image src={thicc} alt="you know what it is" layout="fill" /> */}
+    <div className="bg-grey h-40 md:h-64 grid place-items-center border">
+      <img 
+      src="/images/no-image-2.png" 
+      alt="you know what it is" 
+      className="border w-32" />
     
 
     </div>
--- pages/[skatepark].tsx
+++ pages/[skatepark].tsx
@@ -22,10 +22,11 @@ function Skatepark({ spInfo }) {
           <h1 className="font-bold text-center text-2xl py-4">
             {spInfo.name} Skatepark
           </h1>
-          <div className="relative h-80 mx-auto max-w-3xl">
+          <div className="h-80 border mx-auto max-w-3xl grid place-items-center">
             <img
               src='/images/no-image-2.png'
               alt="you know what it is"
+              className="w-72 border"
             />
           </div>
           <div id="skatepark-info">
@@ -39,7 +40,7 @@ function Skatepark({ spInfo }) {
               <b>Native Land:</b> {spInfo.native_land}
             </p>
             <p className="my-6 mx-4">
-              <b>Rain Cover:</b> {spInfo.rain_cover.toString()}
+              <b>Rain Cover:</b> {spInfo.rain_cover.toString() == true ? 'protected from rain': 'nope'}
             </p>
             <p className="my-6 mx-4">
               <b>Features:</b> soon....
@@ -51,7 +52,7 @@ function Skatepark({ spInfo }) {
 
             <Splide
             className="mt-4"
-            tag="section"
+           
             options={{
               autoplay: true,
               arrows: false,
